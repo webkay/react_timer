00131129f84f77ab7109830f1e89f9ae931383d0
websitekay <websitekay@gmail.com>
2017-02-18 01:24:05 -0800

    Add page Timer and Countdown specific components

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -8,7 +8,7 @@ var Navigation = () => {
         <ul className="menu">
           <li className="menu-text">React Timer App</li>
           <li><IndexLink to="/" activeClassName="active-link">Timer</IndexLink></li>
-          <li><Link to="/" activeClassName="active-link">About</Link></li>
+          <li><Link to="/countdown" activeClassName="active-link">Countdown</Link></li>
         </ul>
       </div>
       <div className="top-bar-right">
---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require('react')
+
+var Timer = (props) => {
+  return (
+    <div>
+      <h1 className="text-center">Timer.jsx</h1>
+    </div>
+  )
+}
+
+module.exports = Timer
